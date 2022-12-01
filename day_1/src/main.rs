97a4d558093e91045897b5efa962f5a2392c1c5c
Dylan Jones <dylan.jones@shield.ai>
2022-12-01 13:33:25 -0800

    Adding day 1 part 2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,7 +1,9 @@
 use std::fs;
+use std::collections::BinaryHeap;
 
 fn main() {
     part_1();
+    part_2();
 }
 
 fn part_1() {
@@ -40,4 +42,31 @@ fn part_1() {
 
     println!("{}", max_id);
     println!("{}", max_cals);
+}
+
+fn part_2() {
+    let contents = fs::read_to_string("data/data.txt").expect("Should have been able to read the file");
+
+    let split = contents.lines();
+    
+    let lines = split.collect::<Vec<&str>>();
+
+    let mut cur_cals = 0;
+    let mut heap = BinaryHeap::new();
+
+    // Loop through the lines and add up calories
+    for line in lines {
+        if !line.is_empty(){
+            let cals = line.parse::<i32>().unwrap();
+            cur_cals += cals;
+        } else {
+            heap.push(cur_cals);
+            cur_cals = 0;
+        }
+    }
+
+    // Make sure we don't miss the last elf
+    heap.push(cur_cals);
+    
+    println!("{}", heap.pop().unwrap() + heap.pop().unwrap() + heap.pop().unwrap());
 }
